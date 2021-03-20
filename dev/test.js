f2011a6327ce5cdf08aa38ed0c6bbcbaf22273fe
Kanui <marley.kanui@gmail.com>
2021-03-20 12:50:00 -0500

    blockchain-js - global commit 05 - created blocks and transactions

--- dev/test.js
+++ dev/test.js
@@ -1,7 +1,14 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(2389, 'AKLFDJ092348', 'ADSFWETQ235');
-bitcoin.createNewBlock(238234, 'AKLFDJ092348', 'ARQU0398');
-bitcoin.createNewBlock(23234, 'AKLFDPQEWOIRU09', 'ADSFWEU3489');
-console.log(bitcoin);
+bitcoin.createNewBlock(09823, 'ALKDJSHF09283', 'ADSLKJF38247');
+bitcoin.createNewTransaction(100, 'HEYSKJH78342059', 'YOPOFYEHW89437');
+bitcoin.createNewBlock(2039478, 'SDFJH209384', 'WOEIRUSJHKDF');
+
+bitcoin.createNewTransaction(50, 'HEYSKJH78342059', 'YOPOFYEHW89437');
+bitcoin.createNewTransaction(2000, 'HEYSKJH78342059', 'YOPOFYEHW89437');
+bitcoin.createNewTransaction(10, 'HEYSKJH78342059', 'YOPOFYEHW89437');
+
+bitcoin.createNewBlock(2039473, 'SDFJHsdf209384', 'WOEsdfRUSJHKDF');
+
+console.log(bitcoin.chain[2]);
